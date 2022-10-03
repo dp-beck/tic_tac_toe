7268ada23df90564844ccda606dc33ec306ba9ad
Dan Beck <beckdp01@gmail.com>
2022-10-03 13:05:18 -0400

    refactor code to conform to module patterns better

--- script.js
+++ script.js
@@ -1,52 +1,56 @@
-const gameBoard = (() => {
+//TO DO: Implement an AI Option for either player
+//TO DO: Style the display
+//TO DO: Style and Center the Player Marks
+//TO DO: Player's should not be able to make a move after one character wins.
+//TO DO: Create a Reset Button
+
+//TO DO: Put the methods on the prototype.
+const Player = (name, playerPiece, isHuman) => {
+    const checkWin = () => {
+        if ((game.board[0] === playerPiece && game.board[1] === playerPiece && game.board[2] === playerPiece) ||
+            (game.board[3] === playerPiece && game.board[4] === playerPiece && game.board[5] === playerPiece) ||
+            (game.board[6] === playerPiece && game.board[7] === playerPiece && game.board[8] === playerPiece) ||
+            (game.board[0] === playerPiece && game.board[3] === playerPiece && game.board[6] === playerPiece) ||
+            (game.board[1] === playerPiece && game.board[4] === playerPiece && game.board[7] === playerPiece) ||
+            (game.board[2] === playerPiece && game.board[5] === playerPiece && game.board[8] === playerPiece) ||
+            (game.board[0] === playerPiece && game.board[4] === playerPiece && game.board[8] === playerPiece) ||
+            (game.board[2] === playerPiece && game.board[4] === playerPiece && game.board[6] === playerPiece)) 
+            {
+              game.displayMessage = `${name} wins!`;
+            } else if (game.movesPlayed == 9) {
+                game.displayMessage = "It's a Tie!";
+            }
+            else {game.displayMessage = `${game.nextTurn.name}'s Turn`;}
+    }
+    const makeMove = (position) => {
+        game.board[position] = playerPiece;
+        checkWin(); 
+    };
+    return {name, playerPiece, isHuman, makeMove }
+};
+
+//storing the tic tac toe board in an object--module pattern
+const game = (() => {
     const board =  ["", "", "",
                     "", "", "",
                     "", "", ""];
-    return { board };
+    let movesPlayed = 0;
+    let displayMessage = "Player One's Turn";
+    const player1 = Player("Player One", "X", true);
+    const player2 = Player("Player Two", "O", true);
+    let whoseTurn = player1;
+    let nextTurn = player2;
+    return { board, movesPlayed, displayMessage, whoseTurn, nextTurn };
 })();
 
-const Player = (name, playerPiece, humanOrComputer) => {
-    const getName = () => name;
-    const getPlayerPiece = () => playerPiece;
-    const getHumanOrCompluter = () => humanOrComputer;
-    const checkWin = () => {
-        if ((gameBoard.board[0] === playerPiece && gameBoard.board[1] === playerPiece && gameBoard.board[2] === playerPiece) ||
-            (gameBoard.board[3] === playerPiece && gameBoard.board[4] === playerPiece && gameBoard.board[5] === playerPiece) ||
-            (gameBoard.board[6] === playerPiece && gameBoard.board[7] === playerPiece && gameBoard.board[8] === playerPiece) ||
-            (gameBoard.board[0] === playerPiece && gameBoard.board[3] === playerPiece && gameBoard.board[6] === playerPiece) ||
-            (gameBoard.board[1] === playerPiece && gameBoard.board[4] === playerPiece && gameBoard.board[7] === playerPiece) ||
-            (gameBoard.board[2] === playerPiece && gameBoard.board[5] === playerPiece && gameBoard.board[8] === playerPiece) ||
-            (gameBoard.board[0] === playerPiece && gameBoard.board[4] === playerPiece && gameBoard.board[8] === playerPiece) ||
-            (gameBoard.board[2] === playerPiece && gameBoard.board[4] === playerPiece && gameBoard.board[6] === playerPiece)) 
-            {
-              displayMessage = `${getName()} wins!`;
-            } else if (counter == 9) {
-                displayMessage = "It's a Tie!";
-            }
-            else {displayMessage = `${nextTurn.getName()}'s Turn`;}
-    }
-    const makeMove = (position) => {
-        gameBoard.board[position] = getPlayerPiece();
-        checkWin(); 
-    };
-    return {getName, getPlayerPiece, makeMove }
-};
-
-const player1 = Player("Player One", "X", "Human");
-const player2 = Player("Player Two", "O", "Human");
-let displayMessage = "Player One's Turn";
-let whoseTurn = player1;
-let nextTurn = player2;
-let temp;
-let counter = 0;
-
+//TO DO = Do I need those extra set of parentheses?
 const displayController = ((clickedId) => {
-    counter ++;
-    whoseTurn.makeMove(parseInt(clickedId, 10));
-    document.getElementById(clickedId).innerHTML = whoseTurn.getPlayerPiece();
-    document.getElementById("displayMessage").innerHTML = displayMessage;
-    temp = whoseTurn
-    whoseTurn = nextTurn;
-    nextTurn = temp;
+    game.movesPlayed ++;
+    game.whoseTurn.makeMove(parseInt(clickedId, 10));
+    document.getElementById(clickedId).innerHTML = game.whoseTurn.playerPiece;
+    document.getElementById("displayMessage").innerHTML = game.displayMessage;
+    let temp = game.whoseTurn;
+    game.whoseTurn = game.nextTurn;
+    game.nextTurn = temp;
     document.getElementById(clickedId).onclick = "";
 });
