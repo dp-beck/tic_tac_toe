949c451db75e1bc35ddd64d32a7e8455222163a5
Dan Beck <beckdp01@gmail.com>
2022-10-04 21:15:23 -0400

    refactor javascript to follow factory function and module pattern for organization

--- script.js
+++ script.js
@@ -4,53 +4,59 @@
 //TO DO: Player's should not be able to make a move after one character wins.
 //TO DO: Create a Reset Button
 
-//TO DO: Put the methods on the prototype.
-const Player = (name, playerPiece, isHuman) => {
-    const checkWin = () => {
-        if ((game.board[0] === playerPiece && game.board[1] === playerPiece && game.board[2] === playerPiece) ||
-            (game.board[3] === playerPiece && game.board[4] === playerPiece && game.board[5] === playerPiece) ||
-            (game.board[6] === playerPiece && game.board[7] === playerPiece && game.board[8] === playerPiece) ||
-            (game.board[0] === playerPiece && game.board[3] === playerPiece && game.board[6] === playerPiece) ||
-            (game.board[1] === playerPiece && game.board[4] === playerPiece && game.board[7] === playerPiece) ||
-            (game.board[2] === playerPiece && game.board[5] === playerPiece && game.board[8] === playerPiece) ||
-            (game.board[0] === playerPiece && game.board[4] === playerPiece && game.board[8] === playerPiece) ||
-            (game.board[2] === playerPiece && game.board[4] === playerPiece && game.board[6] === playerPiece)) 
+//Storing player actions in this prototype for the player objects.
+const playerActions = {
+    checkWin() {
+        if ((game.board[0] === this.piece && game.board[1] === this.piece && game.board[2] === this.piece) ||
+            (game.board[3] === this.piece && game.board[4] === this.piece && game.board[5] === this.piece) ||
+            (game.board[6] === this.piece && game.board[7] === this.piece && game.board[8] === this.piece) ||
+            (game.board[0] === this.piece && game.board[3] === this.piece && game.board[6] === this.piece) ||
+            (game.board[1] === this.piece && game.board[4] === this.piece && game.board[7] === this.piece) ||
+            (game.board[2] === this.piece && game.board[5] === this.piece && game.board[8] === this.piece) ||
+            (game.board[0] === this.piece && game.board[4] === this.piece && game.board[8] === this.piece) ||
+            (game.board[2] === this.piece && game.board[4] === this.piece && game.board[6] === this.piece)) 
             {
-              game.displayMessage = `${name} wins!`;
+              game.displayMessage = `${this.name} wins!`;
             } else if (game.movesPlayed == 9) {
                 game.displayMessage = "It's a Tie!";
             }
             else {game.displayMessage = `${game.nextTurn.name}'s Turn`;}
+    },
+    makeMove(position) {
+        game.board[position] = this.piece;
+        this.checkWin(); 
     }
-    const makeMove = (position) => {
-        game.board[position] = playerPiece;
-        checkWin(); 
-    };
-    return {name, playerPiece, isHuman, makeMove }
 };
 
-//storing the tic tac toe board in an object--module pattern
+const createPlayer = (name, piece, isHuman) => {
+    let player = Object.create(playerActions);
+    player.name = name;
+    player.piece = piece;
+    player.isHuman = isHuman;
+    return player;
+};
+
+//Storing the tic tac toe board in an object--module pattern
 const game = (() => {
     const board =  ["", "", "",
                     "", "", "",
                     "", "", ""];
     let movesPlayed = 0;
     let displayMessage = "Player One's Turn";
-    const player1 = Player("Player One", "X", true);
-    const player2 = Player("Player Two", "O", true);
+    const player1 = createPlayer("Player One", "X", true);
+    const player2 = createPlayer("Player Two", "O", true);
     let whoseTurn = player1;
     let nextTurn = player2;
     return { board, movesPlayed, displayMessage, whoseTurn, nextTurn };
 })();
 
-//TO DO = Do I need those extra set of parentheses?
-const displayController = ((clickedId) => {
+const displayController = (clickedId) => {
     game.movesPlayed ++;
     game.whoseTurn.makeMove(parseInt(clickedId, 10));
-    document.getElementById(clickedId).innerHTML = game.whoseTurn.playerPiece;
+    document.getElementById(clickedId).innerHTML = game.whoseTurn.piece;
     document.getElementById("displayMessage").innerHTML = game.displayMessage;
     let temp = game.whoseTurn;
     game.whoseTurn = game.nextTurn;
     game.nextTurn = temp;
     document.getElementById(clickedId).onclick = "";
-});
+};
